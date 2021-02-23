c45777111f0b1ccad4a692a11121350e7f5ae9fe
pubehunter <manishchandra58@gmail.com>
2021-02-23 11:08:35 +0530

    User create function insert on the main branch

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
@@ -23,7 +25,24 @@ module.exports.signIn = function(req, res){
 
 // get the sign up data
 module.exports.create = function(req, res){
-    // TODO later
+    console.log(req.cookie);
+if(req.body.password != req.body.confirm_password){
+    return res.redirect('back');
+}
+User.findOne({email: req.body.email}, function(err,user){
+    if(err){console.log("error in signing up",err); return}
+
+    if(!user){
+        User.create(req.body,function(err,user){
+            if(err){console.log("Error in creating",err); return}
+
+            return res.redirect("/users/sign-in");
+        });
+    }
+    else{
+        return res.redirect("back");
+    }
+});
 }
 
 
