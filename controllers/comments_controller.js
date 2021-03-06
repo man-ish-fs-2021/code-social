20313cc432daaa3d76100b64ebc301210e871da4
pubehunter <manishchandra58@gmail.com>
2021-03-06 21:26:40 +0530

    Added delete fucntionality to posts and comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -27,3 +27,20 @@ module.exports.create = function (req, res) {
         }
     });
 }
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+        if(comment.user==req.user.id){
+            var postId = comment.post;
+            comment.remove();
+            Post.findByIdAndUpdate(postId,{$pull:{comments: req.params.id}},function(err){
+                if(err){console.log("Error in pulling the comments"); return}
+                return res.redirect("back");
+            });
+            
+        }
+        else{
+            return res.redirect("back");
+        }
+    });
+}
